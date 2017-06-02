5f06b86456a91f7302caa69ba045687592d58a57
grasscoder <hu600hu@163.com>
2017-06-02 22:17:48 +0800

    6-2
    6-2

--- pso.py
+++ pso.py
@@ -4,29 +4,13 @@ import copy
 import numpy as np  
 import matplotlib.pyplot as plt
 from CAPalloction import readFile,classifyUser,getPower,channelAllocate,turnInToParticle
-  
-
-                  
-class Bird(object):##定义一个鸟类（粒子类）
-    
-    def __init__(self, speed, position, fit, lBestPosition, lBestFit):
-        '''
-                         初始化的信息：speed粒子的速率，position：粒子的位置，fit适应度，lBestPostion粒子经理的最佳位置，
-        lBestFit粒子的最佳是硬度值
-        '''
-        self.speed = speed
-        self.position = position
-        self.fit = fit
-        self.lBestFit = lBestFit
-        self.lBestPosition = lBestPosition
 
 #----------------------PSO参数设置---------------------------------  
 class PSO(): 
-     
     def __init__(self,pN,dim,max_iter):  
         self.w = 0.8    
-        self.c1 = 2     
-        self.c2 = 2     
+        self.c1 = 1.4961#2     
+        self.c2 = 1.4961#2     
         self.r1= 0.6  
         self.r2=0.3
         self.channelnum = 64  
@@ -35,15 +19,19 @@ class PSO():
         self.max_iter = max_iter    #迭代次数  
         self.X = np.zeros((self.pN,self.dim))       #所有粒子的位置和速度  
         self.V = np.zeros((self.pN,self.dim))  
-        self.pbest = np.zeros((self.pN,self.dim))   #个体经历的最佳位置和全局最佳位置  
-        self.gbest = np.zeros((1,self.dim))  
+        self.pbest = np.zeros((self.pN,self.dim))   #个体经历的最佳位置 
+        self.gbest = np.zeros((1,self.dim))         #全局最佳位置 
         self.p_fit = np.zeros(self.pN)              #每个个体的历史最佳适应值  
         self.fit = 1e10             #全局最佳适应值  
-          
+        self.filename = ['user.txt','bs.txt']
+        self.UserX,self.UserY,self.BSX,self.BSY = readFile(*self.filename)
+        self.BSCover = classifyUser(100,self.UserX,self.UserY,self.BSX,self.BSY)
+        self.BSchanAllocate=[[-1 for i in xrange(self.channelnum)] for j in xrange(len(self.BSCover))]#初始化信道分配 
 #---------------------目标函数Sphere函数-----------------------------  
     def function(self,x):  #x是列表
         '''
         首要解决的问题是 目标函数的的表达式
+        由于粒子由是不同信道的功率等级（比例）表示的，所以
         '''
         sum = 0  
         length = len(x)  
@@ -54,40 +42,31 @@ class PSO():
 
 #---------------------初始化种群----------------------------------  
     def init_Population(self): 
-        filename = ['user.txt','bs.txt']
-        UserX,UserY,BSX,BSY = readFile(*filename)
-        BSCover = classifyUser(100,UserX,UserY,BSX,BSY)
-        BSchanAllocate=[[-1 for i in xrange(self.channelnum)] for j in xrange(len(BSCover))]#初始化信道分配
         ####将宏基站的坐标加入到基站的坐标列表中
-        BSX = BSX+[0.0]
-        BSY = BSY+[0.0]
+        self.BSX = self.BSX+[0.0]
+        self.BSY = self.BSY+[0.0]
         for i in xrange(5):
-            BSchanAllocate = channelAllocate(BSCover,BSchanAllocate,BSX,BSY)
+            self.BSchanAllocate = channelAllocate(self.BSCover,self.BSchanAllocate,self.BSX,self.BSY)
         
         for i in xrange(self.pN):
-            self.X[i] = turnInToParticle(getPower(BSchanAllocate))#初始化粒子位置
+            self.X[i] = turnInToParticle(getPower(self.BSchanAllocate))#初始化粒子位置
             for j in range(self.dim):
-                self.V[i][j] = random.uniform(0,1) 
-            self.pbest[i] = self.X[i]  
-            tmp = self.function(self.X[i])#目标函数
-            self.p_fit[i] = tmp  
-            if(tmp < self.fit):  
-                self.fit = tmp  
+                self.V[i][j] = random.uniform(0,1) #初始化粒子的速度
+            self.pbest[i] = self.X[i] ##每个粒子的最佳位置 
+            tmp = self.function(self.X[i])#调用目标函数，计算当前粒子的适应值，目标函数是处理每一个粒子的，在这个表达式中显而易见
+            self.p_fit[i] = tmp  ##每个粒子最佳适应值
+            if(tmp < self.fit):  #判断小于全局最佳适应值，将当前粒子的最佳适应值赋值给全局最佳适应值
+                self.fit = tmp   #
                 self.gbest = self.X[i]
-                
-#         for i in range(self.pN):  
-#             for j in range(self.dim):  
-#                 self.X[i][j] = random.uniform(0,1)  #初始化种群的位置
-#                 self.V[i][j] = random.uniform(0,1)  #初始化种群的速度
-#             self.pbest[i] = self.X[i]  
-#             tmp = self.function(self.X[i])  
-#             self.p_fit[i] = tmp  
-#             if(tmp < self.fit):  
-#                 self.fit = tmp  
-#                 self.gbest = self.X[i]  
-#                 
-        
-      
+#--------------------------粒子的惯性权重-----------------------------
+    def inertia_Weight(self,t):
+        """保证惯性权重开始:0.9全局搜索能力强，最后为0.1:局部搜索能力强"""
+        k = (0.9-0.1)/(1-self.max_iter)
+        b = (0.9*self.max_iter-0.1)/(self.max_iter-1)
+        y = k*t + b
+        return y
+    
+         
 #----------------------更新粒子位置----------------------------------  
     def iterator(self):  
         fitness = []  
@@ -97,9 +76,10 @@ class PSO():
                 if(temp<self.p_fit[i]):      #更新个体最优  
                     self.p_fit[i] = temp  
                     self.pbest[i] = self.X[i]  
-                    if(self.p_fit[i] < self.fit):  #更新全局最优  
+                    if(self.p_fit[i] < self.fit):#更新全局最优  
                         self.gbest = self.X[i]  
-                        self.fit = self.p_fit[i]  
+                        self.fit = self.p_fit[i]
+            self.w = self.inertia_Weight(t)##调用类内部的函数,改变w的值，收敛速度明显加快
             for i in range(self.pN):  
                 self.V[i] = self.w*self.V[i] + self.c1*self.r1*(self.pbest[i] - self.X[i])+ self.c2*self.r2*(self.gbest - self.X[i])  
                   
@@ -107,6 +87,7 @@ class PSO():
             fitness.append(self.fit)  
             print(self.fit)                   #输出最优值  
         return fitness
+#----------------------------------粒子群类构造结束-------------------------------------------------
 if __name__=="__main__":    
     #----------------------程序执行-----------------------
     max_iter = 200  
